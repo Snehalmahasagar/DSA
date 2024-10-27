753d04df5bcd6214b219a6f57a4bf4bbab929121
Snehalmahasagar <snehalmahasagar@gmail.com>
2024-10-28 04:09:04 +0530

    array

---
+++ array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int i;
+
+void reverse(int array[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << "  " << array[i] << "  ";
+    }
+}
+
+void printarray(int array[], int n)
+{
+    for (i = 0; i < n; i++)
+    {
+
+        cout << "  " << array[i] << "  ";
+    }
+}
+int main()
+{
+    int array[100];
+    int n;
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+    {
+
+        cin >> array[i];
+    }
+    printarray(array, n);
+
+    reverse(array, n);
+}
