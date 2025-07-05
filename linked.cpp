8893578fc4e096f5cd4df70c76f6b5dbef84216a
Snehal Ajit Mahasagar <snehalmahasagar@gmail.com>
2025-07-05 20:31:10 +0530

    add the changes

--- linked.cpp
+++ linked.cpp
@@ -12,16 +12,24 @@ public:
         next = nullptr;
     }
 };
+void *insert(Node *&head, int val)
+{
+    Node *newnode = new Node(val);
+    newnode->next = head;
+    head = newnode;
+}
 int main()
 {
     Node *head = new Node(1);
     Node *temp = head;
     int n = 5;
+
     for (int i = 2; i <= n; i++)
     {
         temp->next = new Node(i);
         temp = temp->next;
     }
+    insert(head, 0);
 
     temp = head;
     while (temp != nullptr)
@@ -30,5 +38,6 @@ int main()
         cout << temp->data << " ";
         temp = temp->next;
     }
+
     cout << endl;
 }
