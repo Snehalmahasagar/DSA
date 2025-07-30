0634a9d6b9492fd31b6300e065717405af97e023
Snehal Ajit Mahasagar <snehalmahasagar@gmail.com>
2025-07-30 15:34:43 +0530

    added the doubly linked list code

---
+++ dll.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        /* code */
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertEnd(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (!head)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next)
+    {
+        /* code */
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+int main()
+{
+    Node *head = nullptr;
+    insertEnd(head, 10);
+    insertEnd(head, 20);
+    insertEnd(head, 30);
+    insertEnd(head, 40);
+    printList(head);
+    return 0;
+}
